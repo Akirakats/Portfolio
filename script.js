4aef35183e365d7d567684a7a95e6da42b96a892
Diogo <diogo.paula@edu.senai.br>
2026-05-09 17:30:57 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+const botaoMensagem = document.getElementById("botaoMensagem")
+const mensagem = document.getElementById("mensagem")
+
+function alterarTexto(){
+    //mensagem.textContent = "Bem-vindo ao meu portfólio! Este projeto foi criado com HTML, CSS e JavaScript"
+alert("Bem-vindo ao meu portfólio! Este projeto foi criado com HTML, CSS e JavaScript")
+}
+
